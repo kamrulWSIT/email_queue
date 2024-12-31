067ffbeb480a1b710d1d88fa138130f52e786f86
kamrulWSIT <kamrul@workspaceit.com>
2024-12-31 16:54:12 +0600

    faild jobs

---
+++ app/Console/Commands/Generate100QueueJobs.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Generate100QueueJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate100-queue-jobs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
